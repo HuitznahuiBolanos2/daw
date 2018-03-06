c7aed03d8c6d3c66050750225ccd0d0bae34a9bc
huitznahuibolanos <33267939+huitznahuibolanos@users.noreply.github.com>
2018-03-05 21:24:00 -0600

    Agregar lab 15 y 16

---
+++ Laboratorios/Investigacion 2/javaScript/M5C.js
@@ -0,0 +1,10 @@
+document.getElementById("agregarProductoVenta").onclick = agregarProductoVentas;
+var i=1;
+
+function agregarProductoVentas(){
+  /*var clonProd = $("#productoVenta").clone();
+  clonProd.id = "productoVenta" + ++i;
+  $("#camposNuevaVenta").append(clonProd);*/
+
+  var clonTooltip =$("#BorrarProducto")
+}
